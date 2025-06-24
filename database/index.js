e7c8e41cb9d3cc487dc3583f2b122bda41622ba2
slim-clim <110899165+slim-clim@users.noreply.github.com>
2025-06-24 21:06:29 +0100

    Update index.js

--- database/index.js
+++ database/index.js
@@ -6,11 +6,11 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  port: process.env.DATABASE_PORT,
 });
 
 // ✅ Handle connection errors globally
